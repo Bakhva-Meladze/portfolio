ae1f94f96483e2186124fe8c0cda55c6d101b1c5
Bakhva Meladze <88619790+Bakhva-Meladze@users.noreply.github.com>
2023-08-24 22:43:40 +0400

    change header texts

--- translateText.js
+++ translateText.js
@@ -2,7 +2,7 @@
  export const buttonText ={
      aboutMe:["about me","ჩემს შესახებ"],
      mySkills:["My skills","ჩემი უნარები"],
-     myContact:["contact information","საკონტაქტო ინფორმაცია"],
+     myContact:["contact","საკონტაქტო"],
      myProjects:["My projects", "ჩემი პროექტები"],
 }
 
