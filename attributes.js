d6b81bff3cddd0e14fda2d9ed01acd70ad4d5283
Bakhva Meladze <88619790+Bakhva-Meladze@users.noreply.github.com>
2023-11-14 23:15:16 +0400

    popup improvement

--- attributes.js
+++ attributes.js
@@ -2,7 +2,10 @@
 export const arrayOfButtonClass = ["buttonAboutMe","buttonMySkills","buttonContact","myProjects"];
 export const arrayOfDivs = ["aboutMe","mySkills","address","myProject"];
 export const arrayOfTextId = ["name","profession","buttonAboutMe","buttonMySkills","buttonContact","myProjects"];
-
+export const arrayOfProjectButtonID = [
+    "projectOne","projectTwo","projectThree",
+    "projectFour","projectFive","projectSix"
+];
 export const  htmlAttributesOfTranslate = [
     `<div class="divGeo"><img src="images/pngwing.com%20(1).png">ENG</div>`,
     `<div class="divGeo"><img src="images/georgia-flag-icon.png">Geo</div>`,
