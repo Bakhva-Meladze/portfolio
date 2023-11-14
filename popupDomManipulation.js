d89f45cf2da5d039fc9e60e50d41c19df39034c3
Bakhva Meladze <88619790+Bakhva-Meladze@users.noreply.github.com>
2023-11-14 23:17:34 +0400

    popup improvement

---
+++ popupDomManipulation.js
@@ -0,0 +1,34 @@
+import {description} from "./descriptions.js";
+
+class popupDomManipulation {
+
+    constructor(props) {
+        this.state = {
+            logo:null,
+            popup:null,
+            language: localStorage.getItem("id")
+        }
+
+        this.popupVisible = this.popupVisible.bind(this);
+    }
+    data(key) {
+        alert(key);
+    }
+
+    popupVisible (key) {
+        document.getElementById("overlay").style.display ="block";
+        this.state.logo = `<p>Used</p>`+description[key].images.map((value,key)=> {
+            return `<img class="img" src=${value} >`
+        });
+        this.state.popup = `<p id="">${description[key].text[this.state.language]}</p>,
+        <img class="img" src="${description[key].gif}">`;
+        this.renderDom();
+    }
+    renderDom(){
+        document.getElementById('logo').insertAdjacentHTML("afterbegin",this.state.logo);
+        document.getElementById('list').insertAdjacentHTML("afterbegin",this.state.popup);
+
+    }
+}
+
+export default new popupDomManipulation();
