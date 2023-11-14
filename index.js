02ba4d32860d0ff3061001638dcbf08689ed65c3
Bakhva Meladze <88619790+Bakhva-Meladze@users.noreply.github.com>
2023-11-14 23:16:39 +0400

    popup improvement

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
-import {arrayOfButtonClass,arrayOfDivs} from "./attributes.js";
+import {arrayOfButtonClass,arrayOfDivs,arrayOfProjectButtonID} from "./attributes.js";
 import  domManipulation from "./domManipulation.js";
-import  "./selectors.js";
+import  {language,eng,geo,close,overlay} from"./selectors.js";
 import "./passData.js";
+import {description} from "./descriptions.js";
+import popupDomManipulation from "./popupDomManipulation.js";
 
-document.querySelector("#language").addEventListener("click", function(e){
+let translate = {
+    key:0
+}
+
+language.addEventListener("click", function(e){
     domManipulation.selectLanguage();
 });
-document.querySelector("#eng").addEventListener("click",function(e){
+eng.addEventListener("click",function(e){
     domManipulation.changeTranslateMenu(0);
+    translate.key =0;
 });
-document.querySelector("#geo").addEventListener("click",function(e){
+geo.addEventListener("click",function(e){
     domManipulation.changeTranslateMenu(1);
+    translate.key =1;
 });
 addEventListener("load",function(){
     domManipulation.changeTranslateMenu(localStorage.getItem("id"));
@@ -25,6 +33,25 @@ setInterval(function () {
     domManipulation.changeTextTimeInterval(document.getElementById("name"),100,99);
 },1000);
 
+arrayOfProjectButtonID.forEach((value,key) =>{
+    document.getElementById(value).addEventListener("click",function(e){
+        document.getElementById("popup").style.display ="block";
+        document.getElementById("logo").innerHTML =" ";
+        document.getElementById('list').innerHTML = " ";
+        if(description[key].id ===value){
+            popupDomManipulation.popupVisible(key);
+        }
+    });
+});
+
+close.addEventListener("click",function(e){
+    document.getElementById("popup").style.display ="none";
+    document.getElementById("overlay").style.display="none";
+});
+overlay.addEventListener("click",function (e){
+    document.getElementById("popup").style.display ="none";
+    document.getElementById("overlay").style.display="none";
+});
 
 
 
