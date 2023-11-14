5df56438b15a357e463f33cd76f195f4a1128fc2
Bakhva Meladze <88619790+Bakhva-Meladze@users.noreply.github.com>
2023-11-14 23:16:09 +0400

    popup improvement

---
+++ descriptions.js
@@ -0,0 +1,44 @@
+import {descriptionText} from "./translateText.js"
+export const description =[
+    {
+        id: "projectOne",
+        text: descriptionText.jsGame,
+        gif: "./images/game.gif",
+        images:["./images/javascript-39404.png"]
+    },
+    {
+        id:"projectTwo",
+        text: descriptionText.reactShop,
+        gif: "./images/Demo.gif",
+        images: ["./images/css.png","./images/img.png"]
+
+    }
+    ,{
+        id:"projectThree",
+        text: "this is my React project",
+        gif: "./images/Demo.gif",
+        images: ["./images/css.png","./images/img.png"]
+
+    },
+    {
+        id:"projectFour",
+        text: "this is my React project",
+        gif: "./images/Demo.gif",
+        images: ["./images/css.png","./images/img.png"]
+
+    },
+    {
+        id:"projectFive",
+        text: "this is my React project",
+        gif: "./images/Demo.gif",
+        images: ["./images/css.png","./images/img.png"]
+
+    },
+    {
+        id:"projectSix",
+        text: "this is my React project",
+        gif: "./images/Demo.gif",
+        images: ["./images/css.png","./images/javascript-39404.png"]
+
+    }
+]
